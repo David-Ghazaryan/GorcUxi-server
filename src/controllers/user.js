b1cfc19be9b346c9ec1e4c73b8a346fee1c4706f
nikoghosian <vahe20006@gmail.com>
2025-05-25 14:34:18 +0400

    fix

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,14 +1,13 @@
-import { User } from "../models";
+import { User } from '../models/index.js';
 
 export const update = async (req, res, next) => {
   try {
     const { id } = req.user;
-    const { gender, cvUrl, info, city, industryId, scheduleType, salary } =
-      req.body;
+    const { gender, cvUrl, info, city, industryId, scheduleType, salary } = req.body;
 
     await User.update(
       { gender, cvUrl, info, city, industryId, scheduleType, salary },
-      { where: { id } }
+      { where: { id } },
     );
 
     return res.status(200).end();
